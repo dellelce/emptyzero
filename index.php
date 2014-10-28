c5018c7f8413e86f3fdc49736df327ec01cf2f8e
Antonio Dell'Elce <antouk@dellelce.com>
2014-10-28 04:21:16 +0000

    posts loop

--- index.php
+++ index.php
@@ -14,5 +14,8 @@
  ?>
  <div id="primary" class="content-area">
    <main id="main" class="site-main" role="main">
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', get_post_format()); ?>
+<?php endwhile; ?>
    </main><!-- #main -->
  </div><!-- #primary -->
